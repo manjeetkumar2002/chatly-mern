07310ba49a814bd0261fec879d2f94bdb96273c3
Manjeet Kumar <mkj22834@gmail.com>
2026-07-11 08:54:19 +0530

    updated cors origin

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
     origin:[
-        "http://localhost:5173"
+        "http://localhost:5173",
+        "https://chatly-o57e.onrender.com"
     ],
     credentials:true
 }))
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,10 +3,7 @@ import SideBar from '../component/SideBar.jsx'
 import MessageArea from '../component/MessageArea.jsx'
 import { useSelector } from 'react-redux'
 const Home = () => {
-  const {user} = useSelector(state=>state.auth)
-  useEffect(()=>{
-
-  },[user])
+  
   return (
     <div className='flex w-screen h-screen'>
       <SideBar></SideBar>
